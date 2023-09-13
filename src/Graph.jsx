0ae9425d148446a96822974916344361dc1573bd
M Gouri Sankar <92708508+mgs042@users.noreply.github.com>
2023-09-13 16:40:26 +0530

    backend

--- src/Graph.jsx
+++ src/Graph.jsx
@@ -1,41 +1,51 @@
-
-import React, { useEffect, useState } from "react";
-import { BarChart, Bar, XAxis, YAxis, Tooltip ,Area,CartesianGrid,AreaChart} from "recharts";
+import React, { useEffect, useState } from 'react';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { firestore } from './firebase';
+import { AreaChart, Area, XAxis, YAxis, Tooltip, CartesianGrid, defs, linearGradient } from 'recharts';
 import './graph.css';
-const url =
-  "https://mocki.io/v1/91c00047-12fa-4998-a394-df8909d3603c";
 
 export default function Graph() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Define your Firestore collection query here
+  const query = firestore.collection('noise'); // Replace with your collection name
+
+  // Use the useCollectionData hook to fetch Firestore data
+  const [queryData] = useCollectionData(query, { idField: 'data' });
+
   useEffect(() => {
-    fetch(url)
-      .then((r) => r.json())
-      .then(setData);
-  }, []);
+    if (queryData) {
+      setData(queryData);
+      setLoading(false);
+    }
+  }, [queryData]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="container">
-       <AreaChart width={480} height={350} data={data}
-  margin={{ top: 10, right: 10, left: 0, bottom: 0 }}>
-  <defs>
-    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-      <stop offset="95%" stopColor="#8884d8" stopOpacity={0.2}/>
-    </linearGradient>
-    {/* <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-    </linearGradient> */}
-  </defs>
-  <XAxis dataKey="date" />
-  <YAxis dataKey="height"/>
-  <CartesianGrid strokeDasharray="3 3" />
-  <Tooltip />
-  
-  <Area type="monotone" dataKey="height" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
- 
-</AreaChart>
+      <AreaChart
+        width={480}
+        height={350}
+        data={data}
+        margin={{ top: 10, right: 10, left: 0, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0.2} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="date" />
+        <YAxis dataKey="height" />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+
+        <Area type="monotone" dataKey="height" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+      </AreaChart>
     </div>
-    
-   
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD0hnDMTjZybGuNtGTUbF6zoaTWh1c_rLA",
+  authDomain: "csml-2023.firebaseapp.com",
+  databaseURL: "https://csml-2023-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "csml-2023",
+  storageBucket: "csml-2023.appspot.com",
+  messagingSenderId: "435854713954",
+  appId: "1:435854713954:web:d752dd08ccca472d281d3b",
+  measurementId: "G-0303HP86G6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
