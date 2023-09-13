d80e7f5ae7923389c91df7b7ebd44ae43fc00ab0
Ananthutj <ananthutj832@gmail.com>
2023-09-13 11:41:54 +0530

    dashboard

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -5,21 +5,29 @@ import Graph from "./Graph";
 const Dashboard = () => {
   return (
     <div className="dashboard">
-      <div style={{display:'flex'}}>
-        <div class="search-container">
+    <div className="dashboard-content">
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <div className="search-container">
           <div>
-            <i class="fa fa-search search-icon"></i>
+            <i className="fa fa-search search-icon"></i>
             <input
               type="text"
-              class="search-input"
+              className="search-input"
               placeholder="Search"
               style={{ paddingLeft: "3rem" }}
             />
           </div>
         </div>
-        <img src="home-flood" style={{display:'flex'}}></img>
+        <p style={{color:'white',margin:'1rem',alignContent:"space-between"}}>John</p>
+        <div className="profile-container">
+          <img className="profile" src="Avatar.png" alt="Flood" />
+        </div>
+      </div>
+      <div className="text-paragraph">
+       <h1 className="text-dashbaord">Dashboard</h1>
       </div>
     </div>
+  </div>
   );
 };
 
---
+++ src/Dashboard2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard2 = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard2
