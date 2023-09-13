17a10c48c3efbf2488f1cee9e38cb06c2fdf4e11
Fimil Faneea <fimilfaneea@gmail.com>
2023-09-13 09:39:46 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import DividePage from './components/Dividepage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="article-container">
+      <div className="article">
+        <h3>title1</h3>
+        <p>article1</p>
+      </div>
+      <div className="article">
+        <h3>title2</h3>
+        <p>article2</p>
+      </div>
     </div>
   );
 }
